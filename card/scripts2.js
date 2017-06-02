e548699b9fdc49c4034a47a76f2a02ed9469059c
Topu <topu.hit@gmail.com>
2017-06-02 14:20:33 +0600

    a

--- card/scripts2.js
+++ card/scripts2.js
@@ -1,5 +1,15 @@
-$(document).ready(function () {
-    setTimeout(function(){
-        $('.mycard').show();
-   }, 19140);
+// $(document).ready(function () {
+//     setTimeout(function(){
+//         $('.mycard').show();
+//    }, 19140);
+// });
+
+
+$(document).ready(function() {
+    $(".mycard").hide(); 
+document.getElementById('video').addEventListener("timeupdate", function() {
+    if(this.currentTime > 19) {
+$(".mycard").show()
+    }
+});
 });
