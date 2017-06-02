6d965ff58b0e2d5d5b2bded041bfb46d2e09d8f9
Topu <topu.hit@gmail.com>
2017-06-02 13:28:22 +0600

    a

--- card/scripts2.js
+++ card/scripts2.js
@@ -1,30 +1,5 @@
-// $(document).ready(function (){
-//     var video = document.getElementById("video");
-
-//     video.addEventListener("timeupdate", function(){
-//             if (this.currentTime >= 20000) {
-
-//                 $(".container").addClass("flexify");
-//             }
-//         }, false);
-
-//     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-//         $('p#phone').html("Contact<br>+1 609-751-2126");
-//         $('p#address').html("<a href='mailto:hello@bo-won.com'>hello@<br>bo-won.com");
-
-//         setTimeout(function() {
-//             $(".container").addClass("flexify");
-//         }, 0);
-// }
-// });
-
-
-
-
 $(document).ready(function () {
     setTimeout(function(){
         $('.mycard').show();
-        
-
-    }, 19150);
+   }, 19120);
 });
