daea4facd1202877454df74591a64f97e9867e92
Topu <topu.hit@gmail.com>
2017-06-02 17:14:06 +0600

    co

--- card/scripts2.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".mycard").hide(); 
-document.getElementById('video').addEventListener("timeupdate", function() {
-    if(this.currentTime > 18.900) {
-$(".mycard").show()
-    }
-});
-});
