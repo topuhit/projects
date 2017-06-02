de6712e2bf632e91d68914cee6a984d5838529c9
Topu <topu.hit@gmail.com>
2017-06-02 12:35:09 +0600

    corre

--- card/scripts2.js
+++ card/scripts2.js
@@ -1,19 +1,30 @@
-$(document).ready(function (){
-    var video = document.getElementById("video");
+// $(document).ready(function (){
+//     var video = document.getElementById("video");
 
-    video.addEventListener("timeupdate", function(){
-            if (this.currentTime >= 19.200) {
+//     video.addEventListener("timeupdate", function(){
+//             if (this.currentTime >= 20000) {
 
-                $(".container").addClass("flexify");
-            }
-        }, false);
+//                 $(".container").addClass("flexify");
+//             }
+//         }, false);
 
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('p#phone').html("Contact<br>+1 609-751-2126");
-        $('p#address').html("<a href='mailto:hello@bo-won.com'>hello@<br>bo-won.com");
+//     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+//         $('p#phone').html("Contact<br>+1 609-751-2126");
+//         $('p#address').html("<a href='mailto:hello@bo-won.com'>hello@<br>bo-won.com");
 
-        setTimeout(function() {
-            $(".container").addClass("flexify");
-        }, 0);
-}
+//         setTimeout(function() {
+//             $(".container").addClass("flexify");
+//         }, 0);
+// }
+// });
+
+
+
+
+$(document).ready(function () {
+    setTimeout(function(){
+        $('.mycard').show();
+        
+
+    }, 20000);
 });
